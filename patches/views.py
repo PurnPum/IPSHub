dd3c89da22c80b6b353944c324a956df1fd889cf
PurnPum <richy94@hotmail.es>
2024-05-19 23:41:42 +0200

    Main layout for patches created

--- patches/views.py
+++ patches/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def patches_list(request):
-    return render(request, 'patches/patches_sidebar_layout.html')
+    return render(request, 'patches/patches.html')
