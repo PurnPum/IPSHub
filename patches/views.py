98863288cc8f76c28ad5cf460eb3fe66642a6a2c
PurnPum <richy94@hotmail.es>
2024-06-14 06:30:35 +0200

    Added very first version of the filtering system.

--- patches/views.py
+++ patches/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.db.models import Count
 from .models import Patch, PatchOption
 from categories.models import Category
+from games.models import Game
 from . import add_junk_to_db, add_real_data_to_db
 
 def patches_list(request):
@@ -32,7 +34,11 @@ def patches_list(request):
         patch_list.append({
             'patch': patch,
             'game': game,
-            'categories': categories_string
+            'categories': categories_string         
         })
+        
+    top_8_games = Game.objects.annotate(num_patches=Count('categories__patchoption__patches')).order_by('-num_patches')[:8]
+    top_8_categories = Category.objects.annotate(num_categories=Count('patchoption__patches')).order_by('-num_categories')[:8]        
+    top_8_parent_patches = Patch.objects.annotate(num_child_patches=Count('subpatches')).order_by('-num_child_patches')[:8]
    
-    return render(request, 'patches/patches.html', {'patches': patch_list})
+    return render(request, 'patches/patches.html', {'patches': patch_list, 'top8games': top_8_games, 'top8categories': top_8_categories, 'top8parentpatches': top_8_parent_patches})
