aad949c0d7997f7ff69f2cf974cdfdcb66d6a649
PurnPum <richy94@hotmail.es>
2024-09-03 03:16:20 +0200

    Fixed issue_data having an incorrect origin.

--- .github/workflows/python_scripts/parse_issue_form_data.py
+++ .github/workflows/python_scripts/parse_issue_form_data.py
@@ -1,21 +1,19 @@
-import json
 import re
 import os
 
-# Load issue data from the previous step's output
-issue_data = json.loads(os.environ['ISSUE_DATA'])
-body = issue_data['body']
-title = body['title']
-user_login = body['user']['login']
+body = os.environ['ISSUE_BODY']
+title = os.environ['ISSUE_TITLE']
 
 # Extract data from the issue body
-base_game = issue_data['base_game']
-implementer = issue_data['implementer']
-description = re.search(r"^(.*)(:[\s*])(.*)",title).group(3)
+body_pattern = r"(### Suggestion Details.*)(### Base Game\n\n)(.*)(\n\n)(### Implementer\n\n)(.*)"
+title_pattern = r"^(.*)(:[\s*])(.*)"
+
+base_game = re.search(body_pattern,body,re.DOTALL).group(3)
+implementer = re.search(body_pattern,body,re.DOTALL).group(6)
+description = re.search(title_pattern,title).group(3)
 
 # Set outputs for the next steps
 with open(os.environ['GITHUB_OUTPUT'], 'a') as output_file:
   output_file.write(f'base_game={base_game}\n')
   output_file.write(f'implementer={implementer}\n')
   output_file.write(f'description={description}\n')
-  output_file.write(f'issue_author={user_login}\n')
